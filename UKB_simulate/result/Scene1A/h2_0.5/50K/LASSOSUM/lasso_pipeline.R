d0821d8f2ed31683d2475c297e3296b81484574c
Geyu Zhou <eldronzhou@gmail.com>
2021-05-19 13:08:47 -0400

    simulation for lassosum

---
+++ UKB_simulate/result/Scene1A/h2_0.5/50K/LASSOSUM/lasso_pipeline.R
@@ -0,0 +1,49 @@
+
+library(stringr)
+library(lassosum)
+library(data.table)
+library(parallel)
+
+args <- commandArgs(trailingOnly = T)
+j = args[1] # repeat
+h2 = args[2] # h2
+k = args[3] # scene
+sz = args[4] # sample size
+
+ss = fread(paste0("../../../../../summary_stat/", k, "/h2_", h2, 
+		  "/", sz, "/sim_", j, ".PHENO1.glm.linear"))
+
+ref.bfile <- "../../../../../genotype/validate/validate_10K"
+
+LDblocks <- "EUR.hg19"
+
+cor <- p2cor(p=as.numeric(ss$P), n=50e3, sign=ss$BETA)
+idx <- which(is.na(cor))
+
+if (length(idx) != 0) {
+    cor = cor[-idx]
+    ss = ss[-idx,]
+}
+
+s = c(0.2, 0.5, 0.9, 1)
+
+lambda = exp(seq(log(0.001), log(0.1), length.out=20))
+
+cl <- makeCluster(3)
+
+out <- lassosum.pipeline(cor=cor, chr=ss$"#CHROM", pos=ss$POS,
+	A1=ss$A1, ref.bfile=ref.bfile, LDblocks=LDblocks,
+	s=s, lambda=lambda, cluster=cl)
+
+for (i in seq_along(s)) {
+    for (p in seq_along(lambda)) {
+	res = data.frame(SNP=ss$ID, A1=ss$A1,
+			 BETA=out$beta[[i]][,p])
+	if (!all(res$BETA == 0)) {
+		write.table(res, file=paste0("sim_",j,"/s_",s[i],"_lambda_",lambda[p],".txt"),
+		    row.names=F, col.names=T, quote=F, append=F)
+	}
+    }
+}
+
+
---
+++ UKB_simulate/result/Scene1B/h2_0.5/100K/LASSOSUM/lassosum.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+#SBATCH --partition scavenge
+#SBATCH -c 3
+#SBATCH --mem 8g
+#SBATCH --time 1-00:00:00
+#SBATCH --job-name lassosum
+
+i=${SLURM_ARRAY_TASK_ID}
+j=Scene1B
+k=100K
+h2=0.5
+
+module load R
+
+mkdir -p sim_${i}/
+
+Rscript lasso_pipeline.R ${i} 0.5 ${j} ${k}
+
+mkdir -p ../predict/lassosum
+
+Rscript cv.R ${i} ${h2} ${j}
+
+rm -rf sim_${i}
