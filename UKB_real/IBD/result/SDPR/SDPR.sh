3d38f3b98c18b3fd6b58258b56da99b1a24c085f
Geyu Zhou <eldronzhou@gmail.com>
2021-05-19 15:55:42 -0400

    keep MHC

--- UKB_real/IBD/result/SDPR/SDPR.sh
+++ UKB_real/IBD/result/SDPR/SDPR.sh
@@ -7,5 +7,5 @@
 
 i=${SLURM_ARRAY_TASK_ID}
 
-~/SDPR/SDPR -ref_dir ../../../ref/SDOR -valid ../../../genotype/Ukb_imp_v2_hm3.bim -ss ../../summ_stats/PRS_cs.txt  -N 32372 -chr ${i} -out ./res_${i}.txt -a 0.1 -thin 1 -n_threads 3 -mcmc
+~/SDPR/SDPR -ref_dir ../../../ref/SDPR_MHC -valid ../../../genotype/Ukb_imp_v2_hm3.bim -ss ../../summ_stats/SDPR.txt  -N 32372 -chr ${i} -out ./res_${i}.txt -a 0.1 -thin 1 -n_threads 3 -mcmc
 
