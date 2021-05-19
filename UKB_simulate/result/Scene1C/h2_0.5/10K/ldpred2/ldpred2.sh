e20ee562186006edba2fe88114c2a7d413f0ad98
Geyu Zhou <eldronzhou@gmail.com>
2021-05-19 12:44:08 -0400

    simulation for ldpred2

---
+++ UKB_simulate/result/Scene1C/h2_0.5/10K/ldpred2/ldpred2.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+#SBATCH --partition scavenge
+#SBATCH -c 10
+#SBATCH --mem 20g
+#SBATCH --time 1-00:00:00
+#SBATCH --job-name ldpred2
+
+i=${SLURM_ARRAY_TASK_ID}
+j=Scene1C
+h2=0.5
+k=10K
+
+module load R
+
+mkdir -p sim_${i}/
+
+rm -f sim_${i}/.sbk
+
+export OPENBLAS_NUM_THREADS=1
+
+Rscript ldpred2_pipeline.R ${i} 0.5 ${j} ${k}
+
+rm -f sim_${i}/.sbk
+
+mkdir -p ../predict/ldpred2
+
+Rscript cv.R ${i} ${h2} ${j}
+
+rm -f sim_${i}/*.profile
