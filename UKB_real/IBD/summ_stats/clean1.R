d2b7c100591b400ee0231a092e03ae83cb830e1c
Geyu Zhou <eldronzhou@gmail.com>
2021-05-19 15:16:57 -0400

    keep MHC

--- UKB_real/IBD/summ_stats/clean1.R
+++ UKB_real/IBD/summ_stats/clean1.R
@@ -12,14 +12,23 @@ system("~/software/ldsc/ldsc.py --h2 clean.sumstats.gz --ref-ld-chr /ysm-gpfs/pi
 
 # prepare for PRS-CS
 b = read.table("clean.sumstats.gz", header=T, stringsAsFactors=F)
-snp = read.table("~/scratch60/UKB_real/snp_list/1kgma5_prscs_inter.txt", stringsAsFactors=F)
+snp = read.table("../../snp_list/1kgma5_prscs_inter_wMHC.txt", stringsAsFactors=F)
 b = b[b$SNP %in% snp$V1,]
 b$P = 2*pnorm(-abs(b$Z))
 colnames(b) = c("SNP","A1","A2","BETA","N","P")
 write.table(b[,c(1,2,3,4,6)], file="PRS_cs.txt", append=F, sep="\t", quote=F, row.names=F, col.names=T)
 
+# prepare for SDPR
+write.table(b[,c(1,2,3,4,6,5)], file="SDPR.txt", append=F, sep="\t", quote=F, row.names=F, col.names=T)
+
+# prepare for gctb
+d = a[a$SNP %in% b$SNP,]
+d = d[,c("SNP","A1","A2","FRQ_U_21770","log_odds","SE","P","N")]
+d$SE = abs(d$log_odds / qnorm(d$P/2))
+write.table(d, file="gctb.ma", row.names=F, col.names=T, quote=F, sep="\t", append=F)
+
 # prepare for LDpred
-bim = read.table("../../ref/1000G/eur_SNPmaf5_nomhc.bim", header=F, stringsAsFactors=F)
+bim = read.table("/ysm-gpfs/pi/zhao/gz222/1000g_phase3/genotype_1KG_eur_SNPmaf5/hm3/eur_SNPmaf5.bim", header=F, stringsAsFactors=F)
 b = dplyr::left_join(b, bim, by=c("SNP"="V2"))
 tmp = b[,c(7,9,1:4,6)]
 colnames(tmp)[1:2] = c("CHR", "POS")
